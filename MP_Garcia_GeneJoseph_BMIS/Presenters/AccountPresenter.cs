82bb18e9272266e0232002b2d55918415e5d13ab
gn-grc <genejogarcia.gg@gmail.com>
2020-10-06 21:14:33 +0800

    init LoginHelper

--- MP_Garcia_GeneJoseph_BMIS/Presenters/AccountPresenter.cs
+++ MP_Garcia_GeneJoseph_BMIS/Presenters/AccountPresenter.cs
@@ -11,9 +11,10 @@ namespace MP_Garcia_GeneJoseph_BMIS.Presenters
     class AccountPresenter
     {
 
-        public void Login(IAccount login)
+        public void Login(IAccount loginCredentials)
         {
             Console.WriteLine("Logged User\n" + UserSession.User.Username);
+            
         }
 
     }
--- MP_Garcia_GeneJoseph_BMIS/Views/IAccount.cs
+++ MP_Garcia_GeneJoseph_BMIS/Views/IAccount.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MP_Garcia_GeneJoseph_BMIS.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,6 @@ namespace MP_Garcia_GeneJoseph_BMIS.Views
 {
     interface IAccount
     {
-        IAccount Account { get; set; }
+        Account Account { get; set; }
     }
 }
