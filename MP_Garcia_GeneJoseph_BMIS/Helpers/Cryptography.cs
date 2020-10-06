60ef326eec770571b026718889f2cc58e3ee7780
gn-grc <genejogarcia.gg@gmail.com>
2020-10-06 22:17:49 +0800

    init cryptography class

---
+++ MP_Garcia_GeneJoseph_BMIS/Helpers/Cryptography.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP_Garcia_GeneJoseph_BMIS.Helpers
+{
+    class Cryptography
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>Encrypted password</returns>
+        public static string Encrypt(string password)
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="hashedPassword"></param>
+        /// <returns>Returns the original text of the hashed password</returns>
+        public static string Decrypt(string hashedPassword)
+        {
+            return "";
+        }
+    }
+}
