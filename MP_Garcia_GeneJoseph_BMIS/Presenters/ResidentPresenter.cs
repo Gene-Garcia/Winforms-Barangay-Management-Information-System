c2e3edb53f5ceefa04f1c4f1153465fe95d4e7ae
gn-grc <genejogarcia.gg@gmail.com>
2020-10-11 18:43:41 +0800

    resident presenter methods for display residents, and update a resident

--- MP_Garcia_GeneJoseph_BMIS/Presenters/ResidentPresenter.cs
+++ MP_Garcia_GeneJoseph_BMIS/Presenters/ResidentPresenter.cs
@@ -18,6 +18,7 @@ namespace MP_Garcia_GeneJoseph_BMIS.Presenters
             new AddResident().RunView();
         }
 
+        /// <param name="newResident">Contains the complete information of the new resident, except for an residentId/param>
         public void PostAddResident(IResident newResident)
         {
             newResident.Resident.ResidentId = dbEnt.Resident.Residents().Max(m => m.ResidentId) + 1;
@@ -37,6 +38,62 @@ namespace MP_Garcia_GeneJoseph_BMIS.Presenters
                 MessageBox.Show("Resident was not added.", "New Resident Record", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 // reload view
                 new ResidentPresenter().GetAddResident();
+            }
+        }
+
+        public void GetDisplayResidents()
+        {
+            // DisplayResidents view = new DisplayResidents();
+            // view.Residents = dbEnt.
+            // view.RunView();
+        }
+
+        /// <summary>
+        /// Displays a specific resident
+        /// </summary>
+        public void GetViewResident(int residentId)
+        {
+            Resident resident = dbEnt.Resident.Residents().Where(m => m.ResidentId == residentId).FirstOrDefault();
+
+            if (resident != null)
+            {
+                // render view
+            }
+            else
+            {
+                MessageBox.Show("Resident cannot be found", "View Resident", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                new ResidentPresenter().GetDisplayResidents();
+            }
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="resident">Contains the updated resident model, and the list of currenet residents</param>
+        public void PostUpdateResident(IResident resident)
+        {
+            // remove the old version resident from residents
+            resident.Residents.Remove( resident.Resident );
+            // re-insert the new version of resident to list
+            resident.Residents.Add( resident.Resident );
+            // update text file
+
+            bool status = dbEnt.Resident.SaveResidents(resident.Residents);
+
+            if (status)
+            {
+                MessageBox.Show("Resident " + resident.Resident.FirstName + " " + resident.Resident.LastName + "'s record was updated successfully.", "Update Resident", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // go back to landing page
+                /* Audit TRAIL RECORD and System PROMPT */
+                new DashboardPresenter().Index();
+
+            }
+            else
+            {
+                MessageBox.Show("Resident " + resident.Resident.FirstName + " " + resident.Resident.LastName + "'s record was not updated.", "Update Resident", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // reload view
+                new ResidentPresenter().GetDisplayResidents();
             }
         }
     }
