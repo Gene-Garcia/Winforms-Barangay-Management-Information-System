7996cb4636e6fca62ac19c1acf41d8ad929b7689
gn-grc <genejogarcia.gg@gmail.com>
2020-10-11 14:45:25 +0800

    global constants

---
+++ MP_Garcia_GeneJoseph_BMIS/Helpers/SystemConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP_Garcia_GeneJoseph_BMIS.Helpers
+{
+    class SystemConstants
+    {
+        // Acount Constants
+        const string ACCOUNT_STATUS_ACTIVE = "ACTIVE";
+        const string ACCOUNT_STATUS_ARCHIVED = "ARCHIVED";
+
+        // Resident Constants
+        const string RESIDENT_STATUS_ALIVE = "ALIVE";
+        const string RESIDENT_STATUS_DECEASED = "DECEASED";
+    }
+}
