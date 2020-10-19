511f2adaf75d1613f125ce2be33fddbbbad6c341
gn-grc <genejogarcia.gg@gmail.com>
2020-10-19 20:49:28 +0800

    removed old runview

--- MP_Garcia_GeneJoseph_BMIS/Views/AccountView/RegisterAccountView.cs
+++ MP_Garcia_GeneJoseph_BMIS/Views/AccountView/RegisterAccountView.cs
@@ -22,37 +22,6 @@ namespace MP_Garcia_GeneJoseph_BMIS.Views.AccountView
         private Resident resident = new Resident();
         public Resident Resident { get { return resident; } set { resident = value; } }
 
-        public void RunView()
-        {
-            Console.WriteLine("\n\n\t\tREGISTER NEW ACCOUNT");
-            foreach (var resident in residents)
-            {
-                Console.WriteLine("\tId: {0}", resident.ResidentId);
-                Console.WriteLine("\tName: {0}", resident.FirstName + " " + resident.LastName);
-                Console.WriteLine("\tSex: {0}", resident.Sex);
-                Console.WriteLine("\tBirthday: {0}", resident.Birthdate.ToLongDateString());
-                Console.WriteLine("\tAddress: {0}", resident.Address);
-                Console.WriteLine("\tStatus: {0}\n", resident.Status);
-            }
-
-            do
-            {
-                Console.Write("\tSelect a Resident >>");
-                int residentId = int.Parse(Console.ReadLine());
-
-                // find the resident with the id from the list
-                resident = residents.Where(m => m.ResidentId == residentId).FirstOrDefault();
-
-                if (resident != null)
-                    break;
-                else
-                    Console.WriteLine("\tResident Id not found.\n");
-
-            } while (true);
-
-            new AccountPresenter().PostRegisterAccount(this);
-        }
-
         public void PopulateDataList()
         {
             this.dataList.DataSource = this.residents;
