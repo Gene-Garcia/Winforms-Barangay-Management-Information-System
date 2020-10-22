4bbeabdec4c44fceef8c31dbb9d44b67e9e50fb5
gn-grc <genejogarcia.gg@gmail.com>
2020-10-22 22:16:53 +0800

    audit trail interface

---
+++ MP_Garcia_GeneJoseph_BMIS/Views/IAuditTrail.cs
@@ -0,0 +1,15 @@
+﻿using MP_Garcia_GeneJoseph_BMIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP_Garcia_GeneJoseph_BMIS.Views
+{
+    interface IAuditTrail
+    {
+        AuditTrail AuditTrail { get; set; }
+        List<AuditTrail> AuditTrails { get; set; }
+    }
+}
