966c4a844c6f40ced6be3eced2dbd8acefb326ee
gn-grc <genejogarcia.gg@gmail.com>
2020-10-23 21:14:23 +0800

    updated dashboard model

--- MP_Garcia_GeneJoseph_BMIS/Models/Dashboard.cs
+++ MP_Garcia_GeneJoseph_BMIS/Models/Dashboard.cs
@@ -8,14 +8,19 @@ namespace MP_Garcia_GeneJoseph_BMIS.Models
 {
     class Dashboard
     {
+        public int TotalResidents { get; set; }
         public int MaleResidentCount { get; set; }
         public int FemaleResidentCount { get; set; }
+        public int AliveResidents { get; set; }
+        public int DeceasedResidents { get; set; }
 
+        public int RegisteredFamilies { get; set; }
         public int FamilyMemberCateg1 { get; set; }
         public int FamilyMemberCateg2 { get; set; }
         public int FamilyMemberCateg3 { get; set; }
         public int FamilyMemberCateg4 { get; set; }
 
+        public int AverageAge { get; set; }
         public int AgeRangeCateg1 { get; set; }
         public int AgeRangeCateg2 { get; set; }
         public int AgeRangeCateg3 { get; set; }
